b47c69b731b3e7cec5ab1ac29b9ff4432222cd00
cholomia <pocholomia@gmail.com>
2016-11-19 20:25:11 +0800

    Second Commit

--- music/urls.py
+++ music/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
     # /music/<album_id>/favorite/
-    url(r'^(P<album_id>[0-9]+)/favorite/$]', views.favorite, name='favorite'),
+    url(r'^(?P<album_id>[0-9]+)/favorite/]', views.favorite, name='favorite'),
 
 ]
