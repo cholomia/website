c5b54d2e687e2824241a6816d431a2b7a2b1db8c
cholomia <pocholomia@gmail.com>
2016-11-19 19:12:29 +0800

    First commit

---
+++ music/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Album
+
+
+# Create your views here.
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {
+        'all_albums': all_albums
+    })
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {
+        'album': album
+    })
