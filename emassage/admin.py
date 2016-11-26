af00161d8d1b849483618fede9aa36cee78ad7d4
cholomia <pocholomia@gmail.com>
2016-11-26 21:15:02 +0800

    Initial structure for emassage

--- emassage/admin.py
+++ emassage/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from emassage.models import Course
+from emassage.models import Course, Category, Lesson
 
 # Register your models here.
 admin.site.register(Course)
+admin.site.register(Category)
+admin.site.register(Lesson)
--- emassage/models.py
+++ emassage/models.py
@@ -6,8 +6,31 @@ class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     objective = models.TextField(blank=True)
-    sequence = models.IntegerField()
+    sequence = models.IntegerField(unique=True)
     coverImage = models.FileField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    course = models.ForeignKey(Course, related_name='categories', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    objective = models.TextField(blank=True)
+    sequence = models.IntegerField(unique=True)
+    coverImage = models.FileField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id) + ": " + self.title
+
+
+class Lesson(models.Model):
+    category = models.ForeignKey(Category, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    sequence = models.IntegerField(unique=True)
+    coverImage = models.FileField(blank=True, null=True)
+    pdf = models.FileField()
+
+    def __str__(self):
+        return str(self.id) + ": " + self.title
